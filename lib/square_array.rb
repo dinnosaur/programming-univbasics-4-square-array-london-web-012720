228c4dd86864cc1e3fe21471779e6ba3af8a7312
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-22 00:22:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   counter =0 
-  while counter < array.length
+  while counter < array.length do
   array2 = Array.new
  array2 = array[counter]*array[counter]
   puts array2
